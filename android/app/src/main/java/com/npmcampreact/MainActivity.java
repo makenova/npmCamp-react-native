0a52cd3397ad4b2c575a989117b33e2bef3204da
makenova <themarchmyth@gmail.com>
2016-07-08 22:14:09 -0500

    add a components folder

--- android/app/src/main/java/com/npmcampreact/MainActivity.java
+++ android/app/src/main/java/com/npmcampreact/MainActivity.java
@@ -2,6 +2,8 @@ package com.npmcampreact;
 
 import com.facebook.react.ReactActivity;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -36,6 +38,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new VectorIconsPackage(),
+            new VectorIconsPackage(),
             new VectorIconsPackage()
         );
     }
