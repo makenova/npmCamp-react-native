ae3098df13d077e7c298a1dad0724240624ac4ab
makenova <themarchmyth@gmail.com>
2016-07-09 17:55:34 -0500

    Delete schedule component and work on iOS launch screen

--- components/Schedule/ScheduleNav.js
+++ components/Schedule/ScheduleNav.js
@@ -7,7 +7,6 @@ import {
   View,
 } from 'react-native';
 import { ScheduleList } from './ScheduleList';
-import { Schedule } from './Schedule';
 import { Speaker } from '../Speakers/Speaker';
 import { SpeakersList } from '../Speakers/SpeakersList';
 import { colors } from '../../constants';
@@ -34,8 +33,6 @@ export const ScheduleNav = () => {
     switch (scene.sceneTitle) {
       case 'speaker':
         return <Speaker navigator={navigator} speaker={scene.speaker} />;
-      case 'schedule':
-        return <Schedule navigator={navigator} item={scene.item} />;
       case 'speakerList':
         return <SpeakersList navigator={navigator} speakers={scene.speakers} />;
       case 'scheduleList':
--- components/Speakers/Speaker.js
+++ components/Speakers/Speaker.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import NavigationBar from 'react-native-navbar';
 import { colors } from '../../constants';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.charcoal,
-  },
-  navigationBar: {
-    backgroundColor: colors.red,
-  },
-  navigatorText: {
-    fontFamily: 'Helvetica-Bold',
+    paddingTop: 60,
   },
   speakerContainer: {
     flexDirection: 'column',
     alignItems: 'center',
-    paddingTop: 20,
+    paddingTop: 10,
     marginBottom: 10,
   },
   speakerImage: {
@@ -33,15 +27,6 @@ const styles = StyleSheet.create({
 
 export const Speaker = ({ speaker, navigator }) =>
   <View style={styles.container}>
-    <NavigationBar
-      title={{ title: speaker.name, tintColor: colors.white }}
-      style={styles.navigationBar}
-      leftButton={{
-        title: 'Back',
-        tintColor: colors.yellow,
-        handler: navigator.pop,
-      }}
-    />
     <View style={styles.speakerContainer}>
       <Image source={speaker.image} style={styles.speakerImage} />
       <Text style={styles.speakerBio}>{speaker.name}</Text>
